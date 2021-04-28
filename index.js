a8d98c3b90dd623b70c204a982d973d99c344902
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-28 08:10:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function saturdayFun(activity="..."){
   console.log("This Saturday, I want to ${activity}!");
 }
 saturdayFun("roller-skate");
+function mondayWork(place="..."){
+  console.log("This Monday, I will ${place}");
+}
+mondayWork("go to the office");
