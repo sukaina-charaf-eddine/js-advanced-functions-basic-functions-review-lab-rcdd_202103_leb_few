5d1b7f0e9efbe2f0977ed36f2d15ee20c6b17c1b
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-28 08:14:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 // Your code here
-function saturdayFun(activity){
-  console.log("This Saturday, I want to ${activity}!");
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
 }
-saturdayFun();
-saturdayFun("roller-skate");
-function mondayWork(place="..."){
-  console.log("This Monday, I will ${place}");
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`;
 }
-mondayWork();
-mondayWork("go to the office");
+
+function wrapAdjective(flair="*") {
+  return function(adj="special") {
+    return `You are ${flair }${adj}${flair}!`;
+  }
+}
+
+const Calculator = {
+  add: function(a, b) {
+    return a+b;
+  },
+  subtract: function(a, b) {
+    return a-b;
+  },
+  multiply: function(a, b) {
+    return a*b;
+  },
+  divide: function(a, b) {
+    return a/b;
+  }
+}
+
+function actionApplyer(start, fns) {
+  for (let i = 0; i < fns.length; i++) {
+    start = fns[i](start);
+  }
+  return start;
+}
